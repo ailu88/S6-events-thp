0af8e76b4dc148e04474ab35a060ace411c04856
ailu88 <eloise.emptoz@gmail.com>
2019-02-11 18:16:43 +0100

    add welcome e-mailer

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ApplicationRecord
-
+    after_create :welcome_send
+    
     has_many :attendances
     has_many :attending_events, foreign_key: 'attendant_id', class_name: "Event"
     
     has_many :managed_events, foreign_key: 'admin_id', class_name: "Event"
 
+    
+
+    def welcome_send
+      UserMailer.welcome_email(self).deliver_now
+    end
+  
 end
