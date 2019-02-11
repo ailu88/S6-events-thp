ebe7d58d529a363c7dadb8634d90526f922851c5
ailu88 <eloise.emptoz@gmail.com>
2019-02-11 13:49:15 +0100

    model fix

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+
+    has_many :attendances
     has_many :attending_events, foreign_key: 'attendant_id', class_name: "Event"
+    
     has_many :managed_events, foreign_key: 'admin_id', class_name: "Event"
 
 end
