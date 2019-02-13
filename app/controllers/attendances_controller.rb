16fbc4837464694fdd21378608a334600307cc7d
ailu88 <eloise.emptoz@gmail.com>
2019-02-13 18:18:24 +0100

    heroku fix tentative 1

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -5,9 +5,9 @@ class AttendancesController < ApplicationController
   end
 
   def create
-        @attendance = Attendance.find(params[:id])
-        @user = User.find(@attendance.attendant_id)
-        @event = Event.find(@attendance.event_id)
+        # @attendance = Attendance.find(params[:id])
+        # @user = User.find(@attendance.attendant_id)
+        # @event = Event.find(@attendance.event_id)
   end
 
   def destroy
