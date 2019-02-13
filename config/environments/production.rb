7ca4c97fb6d2bfb40c64c3fd118fa86f1f96cf43
ailu88 <eloise.emptoz@gmail.com>
2019-02-13 13:34:53 +0100

    test heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,6 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  config action_mailer default_url_options = { :host => 'besteventsintown.herokuapp.com'}
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
