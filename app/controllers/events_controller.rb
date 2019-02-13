da8805a1fd6480c207a0452d0cdab5663d63d26c
pierremathieu18@gmail.com <mathieupierre@MacBook-Air-de-MATHIEU.local>
2019-02-13 19:35:03 +0100

    setup views restriction

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,5 @@
 class EventsController < ApplicationController
+   before_action :authenticate_user!, only: [:new]
 
     def index
     	@events = Event.all
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+  before_action :user_match, only: [:show]
 
     def index
     	@users = User.all
@@ -29,5 +31,11 @@ class UsersController < ApplicationController
 	    @user.destroy
 	    redirect_to users_path
   end
+      
+  def user_match
+      if current_user != User.find(params[:id])
+        redirect_to root_path
+      end
+    end
 
 end
