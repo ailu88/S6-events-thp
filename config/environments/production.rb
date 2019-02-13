3fdb4b541077f3857ae5139b8d9b728e3387f90d
ailu88 <eloise.emptoz@gmail.com>
2019-02-13 12:42:01 +0100

    mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,8 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  config action_mailer default_url_options = { :host => 'besteventsintown.herokuapp.com'}
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
